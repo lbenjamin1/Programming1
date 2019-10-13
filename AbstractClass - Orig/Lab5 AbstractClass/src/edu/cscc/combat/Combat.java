00280b59b32469f8d0c97f9d5b2c75adb8a45081
lbenjamin1 <54684824+lbenjamin1@users.noreply.github.com>
2019-10-13 03:16:27 -0400

    Completed Lab 5
    Deleted initial Lab 5.
    Added new, completed Lab 5.
    Name of file was changed.

--- AbstractClass - Orig/Lab5 AbstractClass/src/edu/cscc/combat/Combat.java
+++
@@ -1,8 +0,0 @@
-package edu.cscc.combat;
-
-public interface Combat {
-    int MAX_ATTACK_ROLL = 20;
-    int AUTOMATIC_FAILURE = 1;
-    int AUTOMATIC_SUCCESS = 20;
-    int BASE_DODGE_SCORE = 10;
-}
